6517280df8c17ccb2b082ffb0ed30dc4d73acdf9
martin90909090 <personalmperez@gmail.com>
2023-12-15 20:59:53 -0300

    se definen variables js t_value y valor_total

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,4 +1,8 @@
-let precio = document.getElementById('precio-inicial').textContent;
-let precioSpan = document.querySelector(".precio-inicial");
+const precio = document.getElementById('precio-inicial').textContent;
+let cantidad = document.getElementById("cantidad").textContent;
+let t_value = document.getElementById('valor-total').textContent;
+let valor_total = (precio * cantidad);
 
-precioSpan.innerHTML = precio;
+t_value.innerHTML = valor_total;
+
+console.log(valor_total);
