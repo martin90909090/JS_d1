a786a5ec78efd27628279234c14cb8542e596247
martin90909090 <personalmperez@gmail.com>
2023-12-16 19:54:26 -0300

    se declaran y definen de manera temporal las funciones de click para los botones + y -

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,7 +1,16 @@
 const precio = document.getElementById('precio-inicial').textContent;
 let cantidad = document.getElementById("cantidad").textContent;
 let t_value = document.getElementById('valor-total').textContent;
+let btn_add = document.querySelector('#add');
+let btn_less = document.querySelector('#less');
 let valor_total = (precio * cantidad);
+
+function display() {
+    alert("Clickeado");
+}
+
+btn_add.addEventListener('click', display);
+btn_less.addEventListener('click', display);
 
 t_value.innerHTML = valor_total;
 
